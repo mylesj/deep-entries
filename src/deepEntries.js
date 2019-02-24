11e75e6a21a558030cdfd4c132281951ae57c32e
Myles J <hi@mylesj.dev>
2019-02-24 12:25:13 +0000

    refactor: move transform from deep-entries to iterator

--- src/deepEntries.js
+++ src/deepEntries.js
@@ -1,11 +1,4 @@
 import { deepEntriesIterator } from './deepEntriesIterator'
 
-export const deepEntries = (object, transformFn) => {
-	if (typeof transformFn !== 'function')
-		return Array.from(deepEntriesIterator(object))
-
-	const entries = []
-	for (let entry of deepEntriesIterator(object))
-		entries.push(transformFn(entry))
-	return entries
-}
+export const deepEntries = (object, transformFn) =>
+	Array.from(deepEntriesIterator(object, transformFn))
