59c35d3d55caaf08bbc08d0ca9377cd1773bbc64
Myles J <hi@mylesj.dev>
2019-02-24 12:49:44 +0000

    feat: rotateEntry

--- src/delimitEntry.js
+++ src/delimitEntry.js
@@ -1,7 +1,8 @@
+import { rotateEntry } from './rotateEntry'
+
 export const delimitEntryBy = delimiter => entry => {
-	const keys = entry.slice(0, -1)
-	const value = entry.slice(-1)
-	return [keys.join(delimiter), ...value]
+	const [value, ...keys] = rotateEntry(entry)
+	return [keys.join(delimiter), value]
 }
 
 export const delimitEntry = delimitEntryBy('.')
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-export { delimitEntry, delimitEntryBy } from './delimitEntry'
-export { deepEntriesIterator } from './deepEntriesIterator'
 export { deepEntries } from './deepEntries'
+export { deepEntriesIterator } from './deepEntriesIterator'
+export { delimitEntry, delimitEntryBy } from './delimitEntry'
+export { rotateEntry } from './rotateEntry'
