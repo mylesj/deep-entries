bb0bd64dd7a222a0614a0bdee4749f500c529d3e
Myles J <hi@mylesj.dev>
2019-03-03 21:29:14 +0000

    fix(map-functions): will ignore undefined - i.e. may be composed with a filter

--- src/delimit-entry.js
+++ src/delimit-entry.js
@@ -1,6 +1,7 @@
 import { rotateEntry } from './rotate-entry'
 
 export const delimitEntryBy = delimiter => entry => {
+	if (entry === undefined) return
 	const [value, ...keys] = rotateEntry(entry)
 	return [keys.join(delimiter), value]
 }
--- test/delimit-entry.test.js
+++ test/delimit-entry.test.js
@@ -1,6 +1,12 @@
 import { delimitEntryBy, delimitEntry } from '../src/delimit-entry'
 
 describe('delimitEntryBy', () => {
+	it('should return undefined if the input is undefined', () => {
+		const expected = undefined
+		const actual = delimitEntryBy()(undefined)
+		expect(actual).toBe(expected)
+	})
+
 	it('should return a thunk', () => {
 		const expected = 'function'
 		const actual = typeof delimitEntryBy()
@@ -22,6 +28,12 @@ describe('delimitEntryBy', () => {
 })
 
 describe('delimitEntry', () => {
+	it('should return undefined if the input is undefined', () => {
+		const expected = undefined
+		const actual = delimitEntry(undefined)
+		expect(actual).toBe(expected)
+	})
+
 	it('should concatenate keys in a dot-delimited notation', () => {
 		const input = ['1', '2', '3', 4]
 		const expected = ['1.2.3', 4]
