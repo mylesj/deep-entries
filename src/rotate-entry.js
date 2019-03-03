fa4153e2a365aba37cd51f4c38e612480535b1a8
Myles J <hi@myles.ninja>
2019-03-03 17:43:54 +0000

    feat(rotate-entries): expose rotateEntryBy - close #9

--- src/rotate-entry.js
+++ src/rotate-entry.js
@@ -1,5 +1,8 @@
-export const rotateEntry = entry => {
-	const keys = entry.slice(0, -1)
-	const value = entry.slice(-1)
+export const rotateEntryBy = n => entry => {
+	const mod = (-1 * (Number(n) || 0)) % entry.length
+	const keys = entry.slice(0, mod)
+	const value = entry.slice(mod)
 	return [...value, ...keys]
 }
+
+export const rotateEntry = rotateEntryBy(1)
