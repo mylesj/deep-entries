32ff8af8a73d06358534418ee8a27ec8eb5cf173
Myles J <hi@myles.ninja>
2019-03-03 21:36:07 +0000

    fix(rotate-entry): rotateEntryBy wasn't exported :-/

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 export { deepEntries } from './deep-entries'
 export { deepEntriesIterator } from './deep-entries-iterator'
 export { delimitEntry, delimitEntryBy } from './delimit-entry'
-export { rotateEntry } from './rotate-entry'
+export { rotateEntry, rotateEntryBy } from './rotate-entry'
