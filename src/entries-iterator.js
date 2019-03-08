5fdc4b3ee2a981293e2d39474e9b94f59af9f5c6
Myles J <hi@myles.ninja>
2019-03-08 15:19:19 +0000

    fix: Set / Map cross-window support

--- src/entries-iterator.js
+++ src/entries-iterator.js
@@ -1,9 +1,21 @@
 export function* entriesIterator(input) {
-	if (Array.isArray(input) || input instanceof Map) yield* input.entries()
-	else if (input instanceof Set) {
-		let i = 0
-		for (let value of input) yield [i++, value]
-	} else if (typeof input === 'object')
-		for (let key in input)
-			if (input.hasOwnProperty(key)) yield [key, input[key]]
+	switch (Object.prototype.toString.call(input)) {
+		case '[object Array]':
+			yield* input.entries()
+			break
+
+		case '[object Set]':
+			let i = 0
+			for (let value of input) yield [i++, value]
+			break
+
+		case '[object Map]':
+			yield* input.entries()
+			break
+
+		case '[object Object]':
+			for (let key in input)
+				if (input.hasOwnProperty(key)) yield [key, input[key]]
+			break
+	}
 }
