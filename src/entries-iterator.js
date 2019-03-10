eb71114e8fd8f75c947a54fe6acfb07def775d02
Myles J <hi@myles.ninja>
2019-03-10 13:51:59 +0000

    fix(iterator): support URLSearchParams

--- src/entries-iterator.js
+++ src/entries-iterator.js
@@ -1,16 +1,14 @@
 export function* entriesIterator(input) {
 	switch (Object.prototype.toString.call(input)) {
 		case '[object Array]':
+		case '[object Map]':
+		case '[object URLSearchParams]':
 			yield* input.entries()
 			break
 
 		case '[object Set]':
 			let i = 0
 			for (let value of input) yield [i++, value]
-			break
-
-		case '[object Map]':
-			yield* input.entries()
 			break
 
 		case '[object Object]':
