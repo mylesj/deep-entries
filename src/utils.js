4934f6fe8aa0340c2dd6086947d00540d96a0fb2
Myles J <hi@myles.ninja>
2020-05-25 18:09:31 +0100

    feat: treat regex, dates & boxed values like primitives
    BREAKING CHANGE: regex, date etc. will yield empty when passed directly
    and will be yielded as atomic values when found inside deep structures.

--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
 export const identity = x => x
+
+export const isObjectLike = x => {
+	switch (Object.prototype.toString.call(x)) {
+		case '[object String]':
+		case '[object Number]':
+		case '[object Boolean]':
+		case '[object RegExp]':
+		case '[object Date]':
+			return false
+
+		default:
+			return typeof x === 'object'
+	}
+}
